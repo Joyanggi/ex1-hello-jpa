665986e15bcd43ab87731be1603fe56c5a66ccb2
Joyanggi <jyg8033@gmail.com>
2023-04-26 22:45:02 +0900

    영속성 컨텍스트 2

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,15 +16,13 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println(member.getName());
-            }
+            // 영속
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZ");
+
+
+            System.out.println("=============");
 
             tx.commit();
         }catch(Exception e){
